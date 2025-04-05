74096e2d650a682481dfff6f957efdc8c8041163
JobCorpz <corpuzjerob@gmail.com>
2025-04-06 02:43:13 +0400

    updated header ig

--- restaurant.php
+++ restaurant.php
@@ -102,7 +102,7 @@ function displayStars($rating) {
     <header>
         <div class="header-left">
             <h1>Foodie</h1>
-            <img src="istockphoto-1295311342-612x612.jpg" alt="Foodie Logo" class="logo">
+            <a href="dashboard.php"><img src="istockphoto-1295311342-612x612.jpg" alt="Foodie Logo" class="logo"></a>
         </div>
         <div class="header-right">
             <a href="preference.php"><img src="user-member-avatar-face-profile-icon-vector-22965342.jpg" alt="Profile" class="profile-icon"></a>
